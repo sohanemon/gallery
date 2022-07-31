d558900477d97e5b1bbd4d9ebf6d4c52a7a63a01
sohanemon <m.sohan.rse@gmail.com>
2022-07-31 23:41:03 +0600

    grid layout fixed for gallery

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/html/utils/withMT");
-
-module.exports = withMT({
-  content: ["./index.html"],
+module.exports = {
+  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-});
+};
